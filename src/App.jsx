05c828fda8fdfe3b0e589121195cfcc78f6f5eb6
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-03 13:24:45 +0700

    Working With State

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import styles from "./App.module.css";
 import UserList from "./components/UserList/UserList";
 
@@ -13,18 +13,53 @@ function App() {
 			age: 19,
 		},
 	]);
+
+	const [userName, setUserName] = useState("");
+	const [age, setAge] = useState("");
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		setUsers((prevUsers) => [
+			...prevUsers,
+			{
+				name: userName,
+				age: age,
+			},
+		]);
+		setUserName("");
+		setAge("");
+	};
+
+	const nameChangeHandler = (event) => {
+		setUserName(event.target.value);
+	};
+
+	const ageChangeHandler = (event) => {
+		setAge(event.target.value);
+	};
 	return (
 		<div>
-			<section className={styles.inputSection}>
+			<section className={styles.inputSection} onSubmit={submitHandler}>
 				<form className={styles.userInput}>
 					<div className={styles.dataField}>
 						<div className={styles.inputField}>
 							<label htmlFor="username">Username</label>
-							<input type="text" id="username" />
+							<input
+								type="text"
+								id="username"
+								onChange={nameChangeHandler}
+								value={userName}
+							/>
 						</div>
 						<div className={styles.inputField}>
 							<label htmlFor="age">Age</label>
-							<input type="number" id="age" />
+							<input
+								type="number"
+								id="age"
+								onChange={ageChangeHandler}
+								value={age}
+							/>
 						</div>
 					</div>
 					<div className={styles.button}>
